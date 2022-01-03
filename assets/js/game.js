2ee22384a795fa6907700b8c3cfcb674dafcc7b0
yakattak <stevecalebrese@gmail.com>
2022-01-03 11:21:02 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -65,6 +65,13 @@ var fight = function(enemyName) {
     }
   };
 
+//function to start game
+var startGame = function() {
+
+  //reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
 
 for (var i = 0; i < enemyNames.length; i++) {
     if (playerHealth > 0) {
@@ -83,9 +90,39 @@ for (var i = 0; i < enemyNames.length; i++) {
         // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
         fight(pickedEnemyName);
       } else {
-          window.alert("You have lost your robot in battle! Game over!")
+          window.alert("You have lost your robot in battle! Game over!");
+          
       }
-}
-//fight();
+      
+  }
 
-//LOSE
+  endGame();
+  
+  
+  
+};
+
+
+//EndGame Function
+var endGame = function() {
+  window.alert("The game has now ended. Let's see how you did...");
+  //if player is alive player wins
+  if (playerHealth > 0 ) {
+    window.alert("Great job, you've survived the game! You now have a score of " +playerMoney + ".");
+  } else {
+    window.alert("You've lost your robot in battle.");
+  }
+
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+
+  if (playAgainConfirm) {
+    //restart game
+    startGame();
+  } else {
+    window.alert("Thank you for playing Robot Gladiators. Come back soon!");
+  }
+  
+}
+
+//stargame
+startGame();
